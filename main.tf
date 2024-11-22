05c278da2b1ab296fd24da7e3596dd91885a41d9
S3b3B0khar1 <161499698+S3b3B0khar1@users.noreply.github.com>
2024-11-22 09:39:35 -0800

    Update main.tf with new values by saba

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.9.2" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.77.0"
      }
     }
    }
@@ -14,6 +14,6 @@ provider "aws" {
 	}
 
 resource "aws_instance" "Raminfra" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
+	ami = "ami-012967cc5a8c9f891" 
+	instance_type = "t2.nano"
 }
